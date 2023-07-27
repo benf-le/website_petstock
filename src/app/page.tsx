739af4a7d8b4d0b525e91db33f710ca5b7d2079b
benf-le <banglebang2016@gmail.com>
2023-07-28 06:46:00 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -203,44 +203,76 @@ export default async function Home() {
       </div>
 
       <product>
-        <h1 className="py-5 pl-48 text-2xl font-semibold">
+        <h1 className="py-10 pl-48 text-2xl font-semibold">
           Get more for less with Petstock
         </h1>
 
         <card className="flex w-screen flex-nowrap justify-center">
-          <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card ml-5 w-72 bg-base-100 shadow-xl">
             <figure>
               <img src="/./logo_petstock.png" alt="Shoes" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
+              <h2 className="card-title text-base">
+                Royal Canin Intense Beauty Adult In Jelly Wet Cat Food 85G
+              </h2>
+              <p className="text-sm">RC cat 85g $2.85ea when you buy 36</p>
+              <p className="py-8 text-3xl font-semibold">$3.45</p>
+              <div className="card-actions ">
+                <button className="p-btn-addToCart btn btn-primary">
+                  Add to Card
+                </button>
               </div>
             </div>
           </div>
-          <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card ml-5 w-72 bg-base-100 shadow-xl">
             <figure>
               <img src="/./logo_petstock.png" alt="Shoes" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
+              <h2 className="card-title text-base">
+                Royal Canin Intense Beauty Adult In Jelly Wet Cat Food 85G
+              </h2>
+              <p className="text-sm">RC cat 85g $2.85ea when you buy 36</p>
+              <p className="py-8 text-3xl font-semibold">$3.45</p>
+              <div className="card-actions ">
+                <button className="p-btn-addToCart btn btn-primary">
+                  Add to Card
+                </button>
               </div>
             </div>
           </div>
-          <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card ml-5 w-72 bg-base-100 shadow-xl">
             <figure>
               <img src="/./logo_petstock.png" alt="Shoes" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
+              <h2 className="card-title text-base">
+                Royal Canin Intense Beauty Adult In Jelly Wet Cat Food 85G
+              </h2>
+              <p className="text-sm">RC cat 85g $2.85ea when you buy 36</p>
+              <p className="py-8 text-3xl font-semibold">$3.45</p>
+              <div className="card-actions ">
+                <button className="p-btn-addToCart btn btn-primary">
+                  Add to Card
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="card ml-5 w-72 bg-base-100 shadow-xl">
+            <figure>
+              <img src="/./logo_petstock.png" alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-base">
+                Royal Canin Intense Beauty Adult In Jelly Wet Cat Food 85G
+              </h2>
+              <p className="text-sm">RC cat 85g $2.85ea when you buy 36</p>
+              <p className="py-8 text-3xl font-semibold">$3.45</p>
+              <div className="card-actions ">
+                <button className="p-btn-addToCart btn btn-primary">
+                  Add to Card
+                </button>
               </div>
             </div>
           </div>
