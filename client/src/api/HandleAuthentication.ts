e08411f447840c63e7d5d686a8579ff28ad98e01
benf-le <banglebang2016@gmail.com>
2023-08-05 04:14:08 +0700

    update

---
+++ client/src/api/HandleAuthentication.ts
@@ -0,0 +1,14 @@
+import axiosClient from "@/api/axiosClient";
+import {BASE_URL} from "@/constant/appInfo";
+import {Authentication} from "@/models/Authentication";
+
+class HandleAuthentication {
+    static postLogin = async (url: string, data: Authentication[])=>{
+        return axiosClient.post(BASE_URL +url,data)
+    }
+
+
+}
+
+
+export default HandleAuthentication
--- client/src/app/account/login/page.tsx
+++ client/src/app/account/login/page.tsx
@@ -1,49 +1,51 @@
 "use client";
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
 import axios from "axios";
-import { useRouter } from 'next/navigation'; // nếu dùng "use client" thì dùng from 'next/navigation', còn lại thì dùng from 'next/router';
+import { useRouter } from 'next/navigation';
+import {Authentication} from "@/models/Authentication";
+import HandleAuthentication from "@/api/HandleAuthentication"; // nếu dùng "use client" thì dùng from 'next/navigation', còn lại thì dùng from 'next/router';
 
 
 function Page(props) {
     const router = useRouter(); //dùng để quản lý điều hướng trong Next.js
 
-    const [user, setUser] = React.useState({
-        email: '',
-        password: ''
-    })
-    const [error, setError] = React.useState<string>(null)
-
-    //   const [buttonDisabled, setButtonDisabled]= React.useState(false)
+    // const [user, setUser] = React.useState({
+    //     email: '',
+    //     password: ''
+    // })
+    // const [error, setError] = React.useState<string>(null)
+    //
+    // //   const [buttonDisabled, setButtonDisabled]= React.useState(false)
+    // //
+    // //
+    // //
+    // const onLogin = async (e) => {
+    //     e.preventDefault() //chặn reload trang
+    //     try {
+    //         const response = await axios.post(`http://localhost:8000/login`, user)
+    //
+    //         if (response.status === 201) {
+    //             // Đăng ký thành công
+    //             // Sau khi login thành công, điều hướng đến một trang khác
+    //             router.push('/');
+    //
+    //             // window.location.reload(); // Tùy chỉnh theo nhu cầu của bạn
+    //         } else {
+    //             // Xử lý lỗi không rõ ràng từ backend
+    //             setError('Something went wrong. Please try again later.');
+    //         }
     //
     //
-    //
-    const onLogin = async (e) => {
-        e.preventDefault() //chặn reload trang
-        try {
-            const response = await axios.post(`http://localhost:8000/login`, user)
-
-            if (response.status === 201) {
-                // Đăng ký thành công
-                // Sau khi login thành công, điều hướng đến một trang khác
-                router.push('/');
-
-                // window.location.reload(); // Tùy chỉnh theo nhu cầu của bạn
-            } else {
-                // Xử lý lỗi không rõ ràng từ backend
-                setError('Something went wrong. Please try again later.');
-            }
-
-
-        } catch (error) {
-            // Xử lý lỗi từ API NestJS
-            if (error.response) {
-                setError(error.response.data.message); // Lấy thông báo lỗi từ phản hồi API
-            } else {
-                setError('Something went wrong. Please try again later.'); // Lỗi không rõ ràng không có phản hồi từ API
-            }
-        }
-    }
+    //     } catch (error) {
+    //         // Xử lý lỗi từ API NestJS
+    //         if (error.response) {
+    //             setError(error.response.data.message); // Lấy thông báo lỗi từ phản hồi API
+    //         } else {
+    //             setError('Something went wrong. Please try again later.'); // Lỗi không rõ ràng không có phản hồi từ API
+    //         }
+    //     }
+    // }
 
 
     //
@@ -57,6 +59,35 @@ function Page(props) {
 
 
 
+    const [user, setUser] = useState<Authentication[]>([])
+    const [error, setError] = useState<string>(null)
+
+    const onLogin = async ( ) => {
+        const api =`/login`
+            try {
+                const res: any = await HandleAuthentication.postLogin(api,user)
+
+                if (res.status === 201) {
+                    // Đăng ký thành công
+                    // Sau khi login thành công, điều hướng đến một trang khác
+                    router.push('/');
+
+                    // window.location.reload(); // Tùy chỉnh theo nhu cầu của bạn
+                } else {
+                    // Xử lý lỗi không rõ ràng từ backend
+                    setError('Something went wrong. Please try again later.');
+                }
+
+
+            } catch (error) {
+                // Xử lý lỗi từ API NestJS
+                if (error.response) {
+                    setError(error.response.data.message); // Lấy thông báo lỗi từ phản hồi API
+                } else {
+                    setError('Something went wrong. Please try again later.'); // Lỗi không rõ ràng không có phản hồi từ API
+                }
+            }
+    }
 
 
 
@@ -85,7 +116,7 @@ function Page(props) {
                                         id="email"
                                         type="email"
                                         placeholder="Email"
-                                        value={user.email}
+
                                         required={true}
                                         className="input input-bordered"
                                         onChange={(e) => setUser({...user, email: e.target.value})}
@@ -98,7 +129,7 @@ function Page(props) {
                                     <input
                                         id="password"
                                         type="password"
-                                        value={user.password}
+
                                         placeholder="Password"
                                         className="input input-bordered"
                                         onChange={(e) => setUser({...user, password: e.target.value})}
